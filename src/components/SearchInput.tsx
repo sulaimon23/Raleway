57e90cb84218c790ce8686135605b768d28bba6b
Taiwo Sulaimon <tsulaimon96@gmail.com>
2023-02-09 07:43:33 +0100

    feat: project UI completion

---
+++ src/components/SearchInput.tsx
@@ -0,0 +1,54 @@
+import React from "react";
+import styles from "@/styles/Search.module.css";
+import Image from "next/image";
+import { useState } from "react";
+
+//
+//
+const SearchInput = () => {
+    //
+    const [isFocus, setFocus] = useState(true);
+
+    //
+    return (
+        <div className={styles.searchWrapper}>
+            <div className={styles.containInput}>
+                <div className={styles.input}>
+                    <Image
+                        src="/search.svg"
+                        alt="search icon"
+                        width={20}
+                        height={20}
+                        priority
+                    />
+                    <input type="text" placeholder="Search Product" />
+                </div>
+            </div>
+            {isFocus && (
+                <div className={styles.searchResult}>
+                    <div className={styles.titleInfo}>
+                        <h1>Recent searches</h1>
+                        <h1 className={styles.clear}>Clear all</h1>
+                    </div>
+                    <button
+                        tabIndex={1}
+                        type="submit"
+                        className={styles.recent}
+                    >
+                        <h1>Coca cola</h1>
+                        <Image
+                            src="/close.svg"
+                            alt="close icon"
+                            width={20}
+                            height={20}
+                            priority
+                        />
+                    </button>
+                </div>
+            )}
+            <div className={isFocus ? styles.overlay : ""}></div>
+        </div>
+    );
+};
+
+export default SearchInput;
