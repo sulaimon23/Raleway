df4348539437dccb96ea08fce60d8e90d2478bd2
Taiwo Sulaimon <tsulaimon96@gmail.com>
2023-02-09 22:51:29 +0100

    feat: project files clean up

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -2,8 +2,6 @@ import useHttpSearch from "@/hooks/useSearchHook";
 import styles from "@/styles/Search.module.css";
 import { Props } from "@/types.model";
 import Image from "next/image";
-import { useRouter } from "next/router";
-import { useEffect, useState, useRef } from "react";
 
 //
 //
@@ -11,82 +9,22 @@ import { useEffect, useState, useRef } from "react";
 //
 const SearchInput = (props: { param: Props }) => {
     //
-    const searchInput: any = useRef(null);
-    const { inputValue, isFocus, setFocus, onChange, data, setValue } =
-        useHttpSearch(props.param);
-    const router = useRouter();
-    const [recent, setRecent] = useState([]);
-    const [cursor, setCursor] = useState<number>(0);
-    const [isRecent, setIsRecent] = useState(false);
+    const {
+        inputValue,
+        isFocus,
+        setFocus,
+        onChange,
+        data,
+        handleKeyDown,
+        searchInput,
+        isRecent,
+        search,
+        cursor,
+        removeIndex,
+        recent,
+        setIsRecent,
+    } = useHttpSearch(props.param);
 
-    const search = (e: string) => {
-        setValue(e);
-        let namesSet = getStorage();
-        localStorage.setItem(
-            "recentSearch",
-            JSON.stringify(namesSet.slice(0, 5))
-        );
-        router.replace({
-            query: { ...router.query, q: e },
-        });
-        setFocus(false);
-    };
-
-    const getStorage = () => {
-        let storage: string | null = localStorage.getItem(
-            "recentSearch"
-        ) as string;
-        let inputValues = JSON.parse(storage) || [];
-        if (inputValue && inputValue?.length > 0) {
-            inputValues.unshift(inputValue);
-        }
-        let namesSet = Array.from(new Set(inputValues));
-        return namesSet;
-    };
-    //
-    const removeIndex = (index: number) => {
-        let storage: any = localStorage.getItem("recentSearch");
-        let recentSearch: any = JSON.parse(storage);
-        recentSearch.splice(index, 1);
-        localStorage.setItem("recentSearch", JSON.stringify(recentSearch));
-        setRecent(recentSearch);
-    };
-    //
-    const handleKeyDown = (event: any) => {
-        let activeData = isRecent ? recent : data.suggestions;
-        if (event.key === "Enter") {
-            if (activeData.length <= 0) {
-                search(event.target.value);
-                setIsRecent(false);
-                searchInput.current.blur();
-            } else {
-                search(activeData[cursor].text);
-                setIsRecent(false);
-                searchInput.current.blur();
-            }
-        }
-        if (event.keyCode === 38 && cursor > 0) {
-            setCursor((prev) => prev - 1);
-        } else if (event.keyCode === 40 && cursor < activeData.length - 1) {
-            setCursor((prev) => prev + 1);
-        }
-    };
-    //
-    useEffect(() => {
-        // Perform localStorage action
-        let storage: any = localStorage.getItem("recentSearch");
-        let recentSearch: any = JSON.parse(storage);
-        if (
-            (!data.suggestions[0]?.text?.length ||
-                data.suggestions[0]?.text?.length <= 0) &&
-            recentSearch
-        ) {
-            setIsRecent(true);
-            setRecent(recentSearch);
-        } else {
-            setIsRecent(false);
-        }
-    }, [data, inputValue]);
     //
     return (
         <div className={styles.inputWrapper}>
--- src/hooks/useSearchHook.ts
+++ src/hooks/useSearchHook.ts
@@ -1,10 +1,16 @@
 import { Data, Props } from "@/types.model";
-import { useState, ChangeEvent } from "react";
+import { useEffect, useState, useRef, ChangeEvent } from "react";
+import { useRouter } from "next/router";
 import axios from "axios";
 //
 //
 const useHttpSearch = (param: Props) => {
     const [isFocus, setFocus] = useState<boolean>(false);
+    const searchInput: any = useRef(null);
+    const router = useRouter();
+    const [recent, setRecent] = useState([]);
+    const [cursor, setCursor] = useState<number>(0);
+    const [isRecent, setIsRecent] = useState(false);
     const [inputValue, setValue] = useState<string | string[] | null>(
         param.param
     );
@@ -32,6 +38,75 @@ const useHttpSearch = (param: Props) => {
         }
     };
 
+    const search = (e: string) => {
+        setValue(e);
+        let namesSet = getStorage();
+        localStorage.setItem(
+            "recentSearch",
+            JSON.stringify(namesSet.slice(0, 5))
+        );
+        router.replace({
+            query: { ...router.query, q: e },
+        });
+        setFocus(false);
+    };
+
+    const getStorage = () => {
+        let storage: string | null = localStorage.getItem(
+            "recentSearch"
+        ) as string;
+        let inputValues = JSON.parse(storage) || [];
+        if (inputValue && inputValue?.length > 0) {
+            inputValues.unshift(inputValue);
+        }
+        let namesSet = Array.from(new Set(inputValues));
+        return namesSet;
+    };
+    //
+    const removeIndex = (index: number) => {
+        let storage: any = localStorage.getItem("recentSearch");
+        let recentSearch: any = JSON.parse(storage);
+        recentSearch.splice(index, 1);
+        localStorage.setItem("recentSearch", JSON.stringify(recentSearch));
+        setRecent(recentSearch);
+    };
+    //
+    const handleKeyDown = (event: any) => {
+        let activeData = isRecent ? recent : data.suggestions;
+        if (event.key === "Enter") {
+            if (activeData.length <= 0) {
+                search(event.target.value);
+                setIsRecent(false);
+                searchInput.current.blur();
+            } else {
+                search(activeData[cursor].text);
+                setIsRecent(false);
+                searchInput.current.blur();
+            }
+        }
+        if (event.keyCode === 38 && cursor > 0) {
+            setCursor((prev) => prev - 1);
+        } else if (event.keyCode === 40 && cursor < activeData.length - 1) {
+            setCursor((prev) => prev + 1);
+        }
+    };
+    //
+    useEffect(() => {
+        // Perform localStorage action
+        let storage: any = localStorage.getItem("recentSearch");
+        let recentSearch: any = JSON.parse(storage);
+        if (
+            (!data.suggestions[0]?.text?.length ||
+                data.suggestions[0]?.text?.length <= 0) &&
+            recentSearch
+        ) {
+            setIsRecent(true);
+            setRecent(recentSearch);
+        } else {
+            setIsRecent(false);
+        }
+    }, [data, inputValue]);
+
     return {
         inputValue,
         isFocus,
@@ -39,6 +114,14 @@ const useHttpSearch = (param: Props) => {
         onChange,
         data,
         setValue,
+        handleKeyDown,
+        searchInput,
+        isRecent,
+        search,
+        cursor,
+        removeIndex,
+        recent,
+        setIsRecent,
     };
 };
 
